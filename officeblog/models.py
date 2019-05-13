c212849efee093de360d00daaf2c132f32460113
sanpangzi <767306336@qq.com>
2019-05-13 17:35:02 +0800

    Initial commit

---
+++ officeblog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.utils.six import python_2_unicode_compatible
+
+class Post(models.Model):
+    title=models.CharField(max_length=70)
+    body=models.TextField()
+    created_time=models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse('officeblog:pysensing')
+
+# Create your models here.
---
+++ officeblog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+import markdown
+
+
+def pysensing(request):
+    post_list = Post.objects.all().order_by('-created_time')
+
+
+    return render(request, 'pysensing.html', context={'post_list': post_list})
