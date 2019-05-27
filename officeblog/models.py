eb1ecf7d8029f8005f71f9c3250e13c8f7e5b98d
sanpangzi <767306336@qq.com>
2019-05-27 17:45:05 +0800

    modify

--- officeblog/models.py
+++ officeblog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.utils.six import python_2_unicode_compatible
 
 class Post(models.Model):
-    title=models.CharField(max_length=70)
+    title=models.CharField(max_length=300)
     body=models.TextField()
     created_time=models.DateTimeField()
 
